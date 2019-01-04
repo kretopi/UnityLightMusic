1b1c12c5ad37de91fb0ec2acd4c38f64ae43e0ce
Piotr Goj <kretopi@gmail.com>
2019-01-04 19:53:39 +0100

    Add GUI

---
+++ UnityMusic/Assets/OnScreen.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class OnScreen : MonoBehaviour {
+
+	public void PlayMusic(){
+		Hv_UkuleleUnity_AudioLib script = GetComponent<Hv_UkuleleUnity_AudioLib>();
+		script.SendEvent(Hv_UkuleleUnity_AudioLib.Event.Play);
+	}
+
+	public void StopMusic(){
+		Hv_UkuleleUnity_AudioLib script = GetComponent<Hv_UkuleleUnity_AudioLib>();
+		script.SendEvent(Hv_UkuleleUnity_AudioLib.Event.Stop);
+	}
+
+	public void SectionC(){
+		Hv_UkuleleUnity_AudioLib script = GetComponent<Hv_UkuleleUnity_AudioLib>();
+		script.SendEvent(Hv_UkuleleUnity_AudioLib.Event.C);
+	}
+
+	public void SectionE(){
+		Hv_UkuleleUnity_AudioLib script = GetComponent<Hv_UkuleleUnity_AudioLib>();
+		script.SendEvent(Hv_UkuleleUnity_AudioLib.Event.E);
+	}
+}
